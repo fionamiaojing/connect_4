cf3916d1fc5dd1a3ad888281b0f5bd3894964752
clfhhc <clfhhc@gmail.com>
2018-05-24 17:28:03 -0700

    set up the webpack

---
+++ client/src/js/index.js
@@ -0,0 +1,6 @@
+import style from '../main.css';
+import React from 'react';
+import App from './components/app.jsx';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- server.js
+++ server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const bodyParser =  require('body-parser');
+const https = require('https');
 
+//creating server
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.sendfile('./public/index.html');
+    res.end();
+})
+
+const server = https.createServer(app);
+server.listen(3000);
