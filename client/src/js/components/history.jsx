373b5199254de3d498f2cb5b09ea1997ae464f1c
clfhhc <clfhhc@gmail.com>
2018-05-28 12:43:03 -0700

    Added relative time in history

--- client/src/js/components/history.jsx
+++ client/src/js/components/history.jsx
@@ -1,4 +1,24 @@
 import React from 'react';
+import moment from 'moment';
+
+moment.updateLocale('en', {
+    relativeTime : {
+        future: "in %s",
+        past:   "%s ago",
+        s  : 'a few sec',
+        ss : '%d sec',
+        m:  "a min",
+        mm: "%d min",
+        h:  "an hr",
+        hh: "%d hr",
+        d:  "a day",
+        dd: "%d days",
+        M:  "a mon",
+        MM: "%d mon",
+        y:  "a yr",
+        yy: "%d yr"
+    }
+});
 
 const History = (props) => {
     return (
@@ -13,6 +33,7 @@ const History = (props) => {
                             <td>{record.players[0]}</td>
                             <td>:</td>
                             <td>{record.players[1]}</td>
+                            <td className="time">{moment(record.createdAt).fromNow()}</td>
                         </tr>
                     ))}
                     </tbody>
